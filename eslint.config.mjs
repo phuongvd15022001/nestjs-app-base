31e2ca1d2d73531b141f3214eb4a3a48d949992b
PhuongVD <phuongvd15022001@gmail.com>
2025-10-17 19:53:20 +0700

    Update Eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -29,6 +29,10 @@ export default tseslint.config(
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-floating-promises': 'warn',
       '@typescript-eslint/no-unsafe-argument': 'warn',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
     },
   },
 );
