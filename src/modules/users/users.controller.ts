70f161b1d85a5695ab7f763ca3f0118de5f95bba
Dipro <phuongvd@dipro.vn>
2025-10-21 09:33:12 +0700

    Controller

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,10 +1,13 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
+  HttpCode,
   Param,
   ParseIntPipe,
   Post,
+  Put,
   Query,
   UseInterceptors,
 } from '@nestjs/common';
@@ -16,32 +19,14 @@ import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { UsersService } from './users.service';
 import { GetListUsersDto } from './dto/request/get-list-users.dto';
 import { BasePaginationResponseDto } from 'src/shared/dtos/base-pagination.response.dto';
+import { UpdateUserDto } from './dto/request/update-user.dto';
 
 @ApiTags('Users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
-  @Post()
-  createUser(@Body() createUserDto: CreateUserDto) {
-    return `Create user with name: ${createUserDto.name}`;
-  }
-
-  @Post('many')
-  createUsers(@Body() createUsersDto: CreateUsersDto) {
-    return `Create user with name: ${createUsersDto.users[0]?.name} and name: ${createUsersDto.users[1]?.name}`;
-  }
-
-  @UseInterceptors(new TransformInterceptor(UserResponseDto))
-  @Get(':id')
-  @ApiOkResponse({ type: UserResponseDto })
-  findOne(@Param('id', ParseIntPipe) id: number) {
-    const todo = new UserResponseDto();
-    todo.id = id;
-    todo.name = 'Name';
-    return todo;
-  }
-
+  // GET /users
   @UseInterceptors(new TransformInterceptor(UserResponseDto))
   @Get()
   @ApiOperation({ summary: 'Get all users' })
@@ -50,5 +35,51 @@ export class UsersController {
   })
   findAll(@Query() getListUsersDto: GetListUsersDto) {
     return this.userService.findAll({ getListUsersDto });
+  }
+
+  // GET /users/:id
+  @UseInterceptors(new TransformInterceptor(UserResponseDto))
+  @Get(':id')
+  @ApiOperation({ summary: 'Get detail user' })
+  @ApiOkResponse({ type: UserResponseDto })
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.findOne(id);
+  }
+
+  // POST /users
+  @UseInterceptors(new TransformInterceptor(UserResponseDto))
+  @Post()
+  @ApiOperation({ summary: 'Create new user' })
+  @ApiOkResponse({ type: UserResponseDto })
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  // PUT /users/:id
+  @UseInterceptors(new TransformInterceptor(UserResponseDto))
+  @Put(':id')
+  @ApiOperation({ summary: 'Update user' })
+  @ApiOkResponse({ type: UserResponseDto })
+  updateUser(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return this.userService.update(id, updateUserDto);
+  }
+
+  // DELETE /users/:id
+  @Delete(':id')
+  @ApiOperation({ summary: 'Delete user' })
+  @HttpCode(204)
+  removeUser(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.remove(id);
+  }
+
+  // POST /users/many
+  @Post('many')
+  @ApiOperation({ summary: 'Create new user' })
+  @ApiOkResponse({ type: Number })
+  createManyUsers(@Body() createUsersDto: CreateUsersDto) {
+    return this.userService.createMany(createUsersDto);
   }
 }
