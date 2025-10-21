0a087ab4d9d65a2305fd69beb54f5457d077d3dc
Dipro <phuongvd@dipro.vn>
2025-10-21 09:16:49 +0700

    Repository

--- src/modules/users/users.repository.ts
+++ src/modules/users/users.repository.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@nestjs/common';
-import { Prisma, User } from '@prisma/client';
+import { Prisma, PrismaClient, User } from '@prisma/client';
+import { DefaultArgs } from '@prisma/client/runtime/library';
 import { PrismaService } from 'src/services/prisma/prisma.service';
 
 @Injectable()
 export class UsersRepository {
   constructor(private prisma: PrismaService) {}
+
+  async findOne(params: {
+    whereUniqueInput: Prisma.UserWhereUniqueInput;
+    includes?: Prisma.UserInclude;
+  }): Promise<User | null> {
+    const { whereUniqueInput, includes } = params;
+
+    return this.prisma.user.findUnique({
+      where: whereUniqueInput,
+      include: includes,
+    });
+  }
 
   async findAll(params: {
     skip?: number;
@@ -23,6 +36,112 @@ export class UsersRepository {
       where,
       orderBy,
       select,
+    });
+  }
+
+  async count(params: { where?: Prisma.UserWhereInput }): Promise<number> {
+    const { where } = params;
+
+    return this.prisma.user.count({ where });
+  }
+
+  async create(params: { data: Prisma.UserCreateInput }): Promise<User> {
+    const { data } = params;
+
+    return this.prisma.user.create({
+      data,
+    });
+  }
+
+  async update(params: {
+    whereUniqueInput: Prisma.UserWhereUniqueInput;
+    data: Prisma.UserUncheckedUpdateInput;
+  }): Promise<User> {
+    const { whereUniqueInput, data } = params;
+
+    return this.prisma.user.update({
+      data,
+      where: whereUniqueInput,
+    });
+  }
+
+  async delete(params: {
+    whereUniqueInput: Prisma.UserWhereUniqueInput;
+  }): Promise<User> {
+    const { whereUniqueInput } = params;
+
+    return this.prisma.user.delete({
+      where: whereUniqueInput,
+    });
+  }
+
+  async createWithTransaction(params: {
+    transaction: Omit<
+      PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>,
+      '$connect' | '$disconnect' | '$on' | '$transaction' | '$use' | '$extends'
+    >;
+    data: Prisma.UserUncheckedCreateInput;
+  }): Promise<User> {
+    const { transaction, data } = params;
+
+    return transaction.user.create({
+      data,
+    });
+  }
+
+  async updateWithTransaction(params: {
+    transaction: Omit<
+      PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>,
+      '$connect' | '$disconnect' | '$on' | '$transaction' | '$use' | '$extends'
+    >;
+    whereUniqueInput: Prisma.UserWhereUniqueInput;
+    data: Prisma.UserUncheckedUpdateInput;
+  }): Promise<User> {
+    const { transaction, whereUniqueInput, data } = params;
+
+    return transaction.user.update({
+      data,
+      where: whereUniqueInput,
+    });
+  }
+
+  async deleteWithTransaction(params: {
+    transaction: Omit<
+      PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>,
+      '$connect' | '$disconnect' | '$on' | '$transaction' | '$use' | '$extends'
+    >;
+    whereUniqueInput: Prisma.UserWhereUniqueInput;
+  }): Promise<User> {
+    const { transaction, whereUniqueInput } = params;
+    return transaction.user.delete({
+      where: whereUniqueInput,
+    });
+  }
+
+  async createManyWithTransaction(params: {
+    transaction: Omit<
+      PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>,
+      '$connect' | '$disconnect' | '$on' | '$transaction' | '$use' | '$extends'
+    >;
+    data: Prisma.UserUncheckedCreateInput[];
+  }) {
+    const { data, transaction } = params;
+
+    return transaction.user.createMany({
+      data,
+    });
+  }
+
+  async deleteManyWithTransaction(params: {
+    transaction: Omit<
+      PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>,
+      '$connect' | '$disconnect' | '$on' | '$transaction' | '$use' | '$extends'
+    >;
+    where: Prisma.UserWhereInput;
+  }) {
+    const { transaction, where } = params;
+    return transaction.user.deleteMany({
+      where,
     });
   }
 }
